0322b794c67a11841b3e38c18b1bb16c6f274e03
Matheo <matheo_jaouen@natural-solutions.eu>
2015-03-13 12:18:02 +0100

    first commit | yeo && gitignore

---
+++ app/app.js
@@ -0,0 +1,22 @@
+define([],
+function() {
+	var app = {}, Layout = {}, JST = window.JST = window.JST || {};
+
+	app = new Backbone.Marionette.Application();
+	
+	Backbone.Marionette.Renderer.render = function(template, data){
+		if (!JST[template]) throw "Template '" + template + "' not found!";
+		return JST[template](data);
+	};
+	
+	Layout = Backbone.Marionette.LayoutView.extend({
+				el : '#main',
+				template: "app/templates/main-layout.ejs",						
+			});
+
+	layout = new Layout();
+	
+	layout.render();
+	
+	return app;
+});
---
+++ app/main.js
@@ -0,0 +1,58 @@
+require.config({ 
+	baseUrl: "app",
+    paths: {
+		'router'				: 'router',
+		'app'					: 'app',
+		'templates'				: '../build/templates',
+		'jquery'				: '../bower_components/jquery/dist/jquery',
+		'backbone'				: '../bower_components/backbone/backbone',
+		'underscore'			: '../bower_components/lodash/dist/lodash',
+		'marionette'			: '../bower_components/marionette/lib/core/backbone.marionette',
+		'backbone.babysitter'	: '../bower_components/backbone.babysitter/lib/backbone.babysitter',
+		'backbone.wreqr'		: '../bower_components/backbone.wreqr/lib/backbone.wreqr',
+		'bootstrap'				: '../bower_components/components-bootstrap/js/bootstrap',
+	},
+	shim : {
+		jquery : {
+			exports : 'jQuery'
+		},
+		underscore : {
+			exports : '_'
+		},
+		backbone : {
+			deps : ['jquery', 'underscore'],
+			exports : 'Backbone'
+		},
+		marionette : {
+			deps : ['jquery', 'underscore', 'backbone'],
+			exports : 'Marionette'
+		},
+		bootstrap : {
+			deps : ['jquery'],
+		},
+		app : {
+			deps : ['jquery', 'underscore', 'backbone', 'marionette'],
+		},
+		router : {
+			deps : ['app'],
+		},
+		templates : {
+			deps : ['underscore']}
+		},    
+});
+
+require([
+	"jquery",
+	"backbone",
+	"underscore",
+	"marionette",
+	"app",
+	"router",
+	"templates",
+	"bootstrap",
+],
+function(jquery, backbone, underscore, marionette, app, router) {
+	app.router = new router();
+
+    Backbone.history.start({ pushState: true });
+});
