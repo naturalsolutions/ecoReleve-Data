84061ef844cef037050e6f025b62f0ef8bfa91b8
Matheo <matheo_jaouen@natural-solutions.eu>
2015-03-13 12:19:14 +0100

    add | grunt build

---
+++ build/templates.js
@@ -0,0 +1,11 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["app/templates/main-layout.ejs"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<p>\n\tUnderscore template rendered!\n</p>';
+
+}
+return __p
+};
