fcf71791f07f5f06604677ddda5abf91cdbe5026
romfabbro <romainfabbro@yahoo.fr>
2015-10-08 09:17:05 +0200

    Start Com Track VS ecoRelevé for Release

---
+++ Back/database/ImportERD.sql
@@ -0,0 +1,97 @@
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[MessageERDImportFromTrack]') AND type in (N'P', N'PC'))
+DROP PROCEDURE MessageERDImportFromTrack
+GO
+
+
+CREATE PROCEDURE MessageERDImportFromTrack
+AS
+BEGIN
+	print 'MessageERDImportFromTrack'
+	
+	DECLARE @TInsetedId TABLE
+	(
+	Id INT
+	)
+	
+	BEGIN TRY
+	BEGIN TRAN
+		------------------------- GESTION DES SUJETS -------------------------
+		print 'Individu insertion'
+		-- Insertion des nouveaux sujets
+		INSERT INTO [NewModelERD].[dbo].[Individual]
+			   ([ID]
+			   ,[Name]
+			   ,[creationDate]
+			   ,[Age]
+			   ,[Sex]
+			   ,[Birth_date]
+			   ,[Death_date]
+			   ,[Old_ID]
+			   ,[FK_IndividualType]
+			   ,[Species]
+			   ,Original_ID)
+			   OUTPUT inserted.ID into @TInsetedId
+		SELECT 30000000+M.objectID ,'',Getdate(),'',S.PropValue,CONVERT(smallDATETIME,BD.PropValue,120) ,NULL,NULL,1,E.PropValue,M.objectID
+		FROM  TMessageReceived M 
+		JOIN  TMessageReceivedDetail S ON M.pk_MessageReceived=S.fk_MessageReceived and S.PropName = 'TInd_Sexe'
+		JOIN  TMessageReceivedDetail BD ON M.pk_MessageReceived=BD.fk_MessageReceived and BD.PropName = 'TInd_DateNaissance'
+		JOIN  TMessageReceivedDetail E ON M.pk_MessageReceived=E.fk_MessageReceived and E.PropName = 'TInd_Espece'
+		WHERE Importdate IS NULL AND M.ObjectType ='Individu' AND m.Provenance = 'TRACK'
+		AND NOT EXISTS (SELECT * FROM [Individual] S WHERE [FK_IndividualType] =1 and S.[Old_ID] = m.ObjectId)
+		
+
+		print 'Inserting DYnPropValues'
+		-- INserttion  des propriétées dynamiques 
+		INSERT INTO [IndividualDynPropValue]
+			   (ID,[StartDate]
+			   ,[ValueInt]
+			   ,[ValueString]
+			   ,[ValueDate]
+			   ,[ValueFloat]
+			   ,FK_Individual
+			   ,FK_IndividualDynProp)
+		SELECT 2000000+ row_number() over(order by m.pk_MessageReceived), GETDATE()
+		,CASE WHEN DP.TypeProp = 'entier' THEN CONVERT(int,D.PropValue) ELSE NULL END
+		,CASE WHEN DP.TypeProp = 'string' THEN D.PropValue ELSE NULL END
+		,CASE WHEN DP.TypeProp = 'date' THEN CONVERT(DATETIME,D.PropValue,120) ELSE NULL END
+		,CASE WHEN DP.TypeProp = 'float' THEN CONVERT(float,D.PropValue) ELSE NULL END
+		,I.ID
+		,DP.ID
+		FROM 	TMessageReceived M 
+		JOIN Individual I on I.Original_Id = M.ObjectId
+		JOIN  TMessageReceivedDetail D ON M.pk_MessageReceived=D.fk_MessageReceived
+		JOIN TMessageDynPropvsTrack CDP ON CDP.TrackName = D.PropName
+		JOIN IndividualDynProp DP ON CDP.ERDName = DP.Name
+		WHERE Importdate IS NULL AND M.ObjectType ='individu' AND m.Provenance = 'TRACK'
+		AND i.ID in (select ID FROM @TInsetedId)
+		
+		UPDATE TMessageReceived
+		SET ImportDate=GETDATE()
+		WHERE ObjectType ='Individu'
+		AND ImportDate IS NULL AND Provenance = 'TRACK'
+		
+		commit tran
+	END TRY
+		BEGIN CATCH
+			print 'CATCH'
+			print @@TRANCOUNT
+			IF @@TRANCOUNT >0  ROLLBACK TRAN;
+			print @@TRANCOUNT
+			
+			DECLARE @ErrorMessage NVARCHAR(4000);
+			DECLARE @ErrorSeverity INT;
+			DECLARE @ErrorState INT;
+			
+			SELECT 
+				@ErrorMessage = ERROR_MESSAGE(),
+				@ErrorSeverity = ERROR_SEVERITY(),
+				@ErrorState = ERROR_STATE();
+			
+			RAISERROR (@ErrorMessage, -- Message text.
+					   @ErrorSeverity, -- Severity.
+					   @ErrorState -- State.
+					   );
+		END CATCH	
+	
+
+END
---
+++ Back/database/InsertDynPropvsTrack.sql
@@ -0,0 +1,62 @@
+﻿INSERT INTO [TMessageDynPropvsTrack]
+           ([TrackName]
+           ,ERDName
+           ,TypeProp)
+     VALUES
+           ('TInd_BagueIDRelacher'
+           ,'Release_Ring_Code'
+           ,'string')
+GO
+/*
+INSERT INTO [TMessageDynPropvsTrack]
+           ([TrackName]
+           ,[EColName]
+           ,TypeProp)
+     VALUES
+           ('TInd_DateNaissance'
+           ,'BirthDate'
+		   ,'Date')
+GO*/
+
+/*
+INSERT INTO [TMessageDynPropvsTrack]
+           ([TrackName]
+           ,[EColName]
+           ,TypeProp)
+     VALUES
+           ('TInd_DateSortie'
+           ,'ExitDate'
+           ,'Date')
+
+Go
+*/
+
+INSERT INTO [TMessageDynPropvsTrack]
+           ([TrackName]
+           ,ERDName
+           ,TypeProp)
+     VALUES
+           ('TInd_Espece'
+           ,'Species'
+           ,'string')
+
+
+
+
+INSERT INTO [TMessageDynPropvsTrack]
+           ([TrackName]
+           ,ERDName
+           ,TypeProp)
+     VALUES
+           ('TInd_Puce'
+           ,'Chip_Code'
+           ,'string')
+
+INSERT INTO [TMessageDynPropvsTrack]
+           ([TrackName]
+           ,ERDName
+           ,TypeProp)
+     VALUES
+           ('TInd_Sexe'
+           ,'sex'
+           ,'string')
