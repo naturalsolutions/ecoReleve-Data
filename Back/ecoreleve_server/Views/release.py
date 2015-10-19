70727346d9c1935badc4f3dcea2a839fbe9c4a11
romfabbro <romainfabbro@yahoo.fr>
2015-10-19 13:55:46 +0200

    WS release : generate Protocol and update Indiv

--- Back/ecoreleve_server/Views/release.py
+++ Back/ecoreleve_server/Views/release.py
@@ -2,12 +2,10 @@ from pyramid.view import view_config
 from ..Models import (
     DBSession,
     Individual,
-    IndividualType,
-    IndividualDynPropValue,
-    IndividualDynProp,
-    Individual_Location,
+    Station,
+    Observation,
+    ProtocoleType,
     Sensor,
-    SensorType,
     Equipment,
     IndividualList
     )
@@ -24,9 +22,29 @@ from sqlalchemy.orm import aliased
 from pyramid.security import NO_PERMISSION_REQUIRED
 from traceback import print_exc
 from collections import OrderedDict
-
+import pandas as pd
+from collections import Counter
 
 prefix = 'release/'
+
+@view_config(route_name= prefix+'action', renderer='json', request_method = 'GET', permission = NO_PERMISSION_REQUIRED)
+def getFilters (request):
+    ModuleType = 'IndivReleaseGrid'
+    filtersList = Individual().GetFilters(ModuleType)
+    filters = {}
+    for i in range(len(filtersList)) :
+        filters[str(i)] = filtersList[i]
+    transaction.commit()
+    return filters
+
+def getFields(request) :
+
+    ModuleType = request.params['name']
+    if ModuleType == 'default' :
+        ModuleType = 'IndivReleaseGrid'
+    cols = Individual().GetGridFields(ModuleType)
+    transaction.commit()
+    return cols
 
 @view_config(route_name= prefix+'individuals', renderer='json', request_method = 'GET', permission = NO_PERMISSION_REQUIRED)
 def searchIndiv(request):
@@ -66,3 +84,129 @@ def searchIndiv(request):
     result.append(dataResult)
     transaction.commit()
     return result
+
+
+@view_config(route_name= prefix+'individuals', renderer='json', request_method = 'POST', permission = NO_PERMISSION_REQUIRED)
+def releasePost(request):
+
+    data = request.params.mixed()
+    sta_id = data['StationID']
+    curStation = DBSession.query(Station).get(sta_id)
+    indivList = json.loads(data['IndividualList'])
+
+    data['taxon'] = data['IndividualList'][0]['taxon']
+
+    class protocolList :
+
+        def __init__(self,typeID):
+            self.typeID = typeID
+            self.list_ = []
+
+        def new(self):
+            return Observation(FK_ProtocoleType=self.typeID)
+
+        def getList(self):
+            return self.list_
+
+        def add(self,obs):
+            self.list_.append(obs)
+
+    protoTypes = pd.DataFrame(DBSession.execute(select([ProtocoleType])).fetchall(), columns = ProtocoleType.__table__.columns.keys())
+    vertebrateGrpID = protoTypes.loc[protoTypes['Name'] == 'Vertebrate group','ID'].values[0]
+    vertebrateIndID = protoTypes.loc[protoTypes['Name'] == 'Vertebrate individual','ID'].values[0]
+    biometryID = protoTypes.loc[protoTypes['Name'] == 'Bird Biometry','ID'].values[0]
+    releaseGrpID = protoTypes.loc[protoTypes['Name'] == 'Release Group','ID'].values[0]
+    releaseIndID = protoTypes.loc[protoTypes['Name'] == 'Release Individual','ID'].values[0]
+    equipmentIndID = protoTypes.loc[protoTypes['Name'] == 'Individual equipment','ID'].values[0]
+
+    vertebrateGrp = Observation(FK_ProtocoleType=vertebrateGrpID)
+    releaseGrp = Observation(FK_ProtocoleType=releaseGrpID)
+
+    vertebrateIndList = protocolList(vertebrateIndID)
+    biometryList = protocolList(biometryID)
+    releaseIndList = protocolList(releaseIndID)
+    equipmentIndList = protocolList(equipmentIndID)
+
+    binaryDict = {
+    9: 'Nb_Adult_Indeterminate',
+    10: 'Nb_Adult_Male',
+    12: 'Nb_Adult_Female',
+    17: 'Nb_Juvenile_Indeterminate',
+    18: 'Nb_Juvenile_Male',
+    20: 'Nb_Juvenile_Female',
+    33: 'Nb_Indeterminate'
+    }
+
+    def MoF_AoJ(obj):
+        #### binary ponderation female : 4, male :2 , indeterminateSex : 1, adult:8, juvenile : 16, indeterminateAge : 32
+        curSex = None
+        curAge = None
+        binP = 0
+
+        if obj['Sex'].lower() == 'male':
+            curSex = 'male'
+            binP += 2
+        elif obj['Sex'].lower() == 'female':
+            curSex = 'female'
+            binP += 4
+        else : 
+            curSex == 'Indeterminate'
+            binP += 1
+
+        if obj['Age'].lower() == 'Adult':
+            curAge = 'Adult'
+            binP += 8
+        elif obj['Age'].lower() == 'juvenile':
+            curAge = 'Juvenile'
+            binP += 16
+        else : 
+            curAge == 'Indeterminate'
+            binP += 32
+        return binaryDict[binP]
+
+    binList = []
+
+    for indiv in indivList: 
+        curIndiv = DBSession.query(Individual).get(indiv['ID'])
+        curIndiv.LoadNowValues()
+        curIndiv.UpdateFromJson(indiv)
+
+        indiv['FK_Individual'] = indiv['ID']
+        indiv['FK_Station'] = curStation.ID
+
+        binList.append(MoF_AoJ(indiv))
+        # here add info for Vetebrate individual protocol
+        curVertebrateInd = vertebrateIndList.new()
+        curVertebrateInd.UpdateFromJson(indiv)
+
+        # here add info for Bird Biometry protocol
+        curBiometry = biometryList.new()
+        curBiometry.UpdateFromJson(indiv)
+        biometryList.add(curBiometry)
+
+        # here add info for Release Individual protocol
+        curReleaseInd = releaseIndList.new()
+        curReleaseInd.UpdateFromJson(indiv)
+        releaseIndList.add(curReleaseInd)
+
+        # here add info for Individual Equipment protocol
+        if isinstance(indiv['FK_Sensor'], int):
+            curEquipmentInd = equipmentIndList.new()
+            curEquipmentInd.UpdateFromJson(indiv)
+            equipmentIndList.add(curEquipmentInd)
+
+    dictVertGrp = dict(Counter(binList))
+    vertebrateGrp.UpdateFromJson(dictVertGrp)
+    releaseGrp.UpdateFromJson(data)
+
+    vertebrateGrp.Observation_children.extend(vertebrateIndList.getList())
+    releaseGrp.Observation_children.extend(releaseIndList.getList())
+
+    # finally append all Protocols to Station
+    curStation.Observations.append(vertebrateGrp)
+    curStation.Observations.append(releaseGrp)
+    curStation.Observations.extend(biometryList.getList())
+
+    transaction.commit()
+
+    return {'release':len(releaseIndList.getList())}
