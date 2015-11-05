d8bcf7962854f39a3ac7b332971203e1c235bc45
Gérald Tibi <gerald_tibi@natural-solutions.eu>
2015-11-05 12:18:29 +0100

    fix  merge conflicts

--- Back/database/Export/pr_ExportAllProtocole.sql
+++ Back/database/Export/pr_ExportAllProtocole.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'pr_ExportAllProtocole ')
 	DROP PROCEDURE pr_ExportAllProtocole 
 GO
@@ -22,30 +22,3 @@ BEGIN
 END
 
 
-
-=======
-IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'pr_ExportAllProtocole ')
-	DROP PROCEDURE pr_ExportAllProtocole 
-GO
-
-CREATE PROCEDURE pr_ExportAllProtocole 
-AS
-BEGIN
-
-	DECLARE @ProtocoleType INT
-	Select ID into #ProtList from [NewModelERD].dbo.ProtocoleType
-	--where ID not in (208)
-
-	WHILE EXISTS (select * from #ProtList) 
-	BEGIN
-		SELECT TOP 1 @ProtocoleType=ID FROM #ProtList
-		print @ProtocoleType
-		execute pr_ExportAllProtocole @ProtocoleType
-		DELETE FROM #ProtList WHERE ID=@ProtocoleType
-	END
-
-END
-
-
-
->>>>>>> 774a4ed94d4fd3ace58237eaed991efecfaac75a
