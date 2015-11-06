2ff66f42c4b8fde2939932b8501a3b68a494c7b5
Gérald Tibi <gerald_tibi@natural-solutions.eu>
2015-11-06 15:06:43 +0100

    Removing Head in File

--- Back/database/Export/pr_ExportOneProtocole.sql
+++ Back/database/Export/pr_ExportOneProtocole.sql
@@ -1,9 +1,11 @@
-<<<<<<< HEAD
+
 IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'pr_ExportOneProtocole')
 	DROP PROCEDURE pr_ExportOneProtocole
 GO
 
-CREATE PROCEDURE pr_ExportOneProtocole 
+
+
+CREATE PROCEDURE [dbo].[pr_ExportOneProtocole] 
 	(
 	@ProtocoleType INT
 	)
@@ -32,7 +34,7 @@ BEGIN
 	DECLARE @Req NVARCHAR(MAX)
 	DECLARE @ReqFrom NVARCHAR(MAX)
 	DECLARE @ReqSet NVARCHAR(MAX)
-	SET @Req = ' CREATE UNIQUE CLUSTERED INDEX PK_TProtocole'  +  replace(@ProtocoleName,' ','_') + ' ON TmpObsExport (ID)' 
+	SET @Req = ' CREATE UNIQUE CLUSTERED INDEX PK_TProtocol'  +  replace(@ProtocoleName,' ','_') + ' ON TmpObsExport (ID)' 
 	exec ( @req)
 	IF EXISTS (select * from [NewModelERD].dbo.ObservationDynProp D JOIN [NewModelERD].dbo.ProtocoleType_ObservationDynProp C ON C.FK_ProtocoleType = @ProtocoleType and c.FK_ObservationDynProp =D.ID )
 	BEGIN
@@ -80,89 +82,9 @@ BEGIN
 			exec ( @req)
 		END	
 	END
-	SET @Req = ' IF object_id(''TProtocol_'  +  replace(@ProtocoleName,' ','_') + ''') IS NOT NULL DROP TABLE  TProtocole'  +  replace(@ProtocoleName,' ','_')
+	SET @Req = ' IF object_id(''TProtocol_'  +  replace(@ProtocoleName,' ','_') + ''') IS NOT NULL DROP TABLE  TProtocol_'  +  replace(@ProtocoleName,' ','_')
 	exec ( @req)
-	SET @Req = ' sp_rename ''TmpObsExport'' ,TProtocole'  +  replace(@ProtocoleName,' ','_')
-
-	exec ( @req)
-
-END
-
-
-
-=======
-IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'pr_ExportOneProtocole')
-	DROP PROCEDURE pr_ExportOneProtocole
-GO
-
-CREATE PROCEDURE pr_ExportOneProtocole 
-	(
-	@ProtocoleType INT
-	)
-AS
-BEGIN
-	IF object_id('TmpObsExport') IS NOT NULL
-			DROP TABLE TmpObsExport
-
-
-	DECLARE @ProtocoleName VARCHAR(255)
-
-
-	SELECT @ProtocoleName = Name from [NewModelERD].dbo.ProtocoleType where id=@ProtocoleType
-	print @ProtocoleName
-
-	---------------------- CREATION DE LA TABLE CIBLE
-
-	-- Ges Static Prop
-	select * into TmpObsExport 
-	from [NewModelERD].dbo.Observation O
-	where O.FK_ProtocoleType = @ProtocoleType
-	
-	
-	DECLARE @Req NVARCHAR(MAX)
-	DECLARE @ReqFrom NVARCHAR(MAX)
-	DECLARE @ReqSet NVARCHAR(MAX)
-
-
-	IF EXISTS (select * from [NewModelERD].dbo.ObservationDynProp D JOIN [NewModelERD].dbo.ProtocoleType_ObservationDynProp C ON C.FK_ProtocoleType = @ProtocoleType and c.FK_ObservationDynProp =D.ID )
-	BEGIN
-		-- ALTER WITH DYN PROPS
-		SET @Req = ' ALTER TABLE TmpObsExport ADD@'
-
-		select @Req = @Req + ',    ' +  replace(D.Name,' ','_') + ' ' + replace(replace(d.typeProp,'Integer','INT'),'string','varchar(255)')  
-		from [NewModelERD].dbo.ObservationDynProp D JOIN [NewModelERD].dbo.ProtocoleType_ObservationDynProp C ON C.FK_ProtocoleType = @ProtocoleType and c.FK_ObservationDynProp =D.ID
-
-		SET @Req = replace(@Req,'ADD@,','ADD ')
-		print @Req
-		exec ( @req)
-	
-
-		IF EXISTS(SELECT * from TmpObsExport)
-		BEGIN
-			-- UPDATE DATA FROM DYN PROP
-
-			SET @ReqSet = 'SET@'
-			SET @ReqFrom =''
-
-			SELECT @ReqSet = @ReqSet + ',' + replace(P.Name,' ','_') + '=V.' + replace(P.Name,' ','_'), @ReqFrom = @ReqFrom + ',MAX(CASE WHEN Name=''' +  replace(P.Name,' ','_') + ''' THEN Value' + replace(P.TypeProp,'Integer','Int') + ' ELSE NULL END) ' + replace(P.Name,' ','_')																						
-			from [NewModelERD].dbo.ObservationDynProp P JOIN [NewModelERD].dbo.ProtocoleType_ObservationDynProp C ON C.FK_ProtocoleType = @ProtocoleType and c.FK_ObservationDynProp =P.ID 
-
-
-			SET @ReqSet = replace(@ReqSet,'SET@,','SET ')
-			print @ReqFrom
-
-			SET @Req = 'UPDATE EI ' + @ReqSet +  ' FROM TmpObsExport EI JOIN (SELECT VN.FK_Observation ' + @ReqFrom + ' FROM   [NewModelERD].dbo.ObservationDynPropValuesNow VN GROUP BY VN.FK_Observation) V ON EI.ID = V.FK_Observation '
-
-			--SET @Req = 'SELECT * FROM TmpIndivExport EI JOIN (SELECT VN.FK_Individual ' + @ReqFrom + ' FROM   [NewModelERD].dbo.IndividualDynPropValuesNow VN GROUP BY VN.FK_Individual) V ON EI.ID = V.FK_Individual '
-
-			print @req
-
-			exec ( @req)
-		END	
-	END
-	SET @Req = ' IF object_id(''TProtocole'  +  replace(@ProtocoleName,' ','_') + ''') IS NOT NULL DROP TABLE  TProtocole'  +  replace(@ProtocoleName,' ','_')
-	exec ( @req)
-	SET @Req = ' sp_rename ''TmpObsExport'' ,TProtocole'  +  replace(@ProtocoleName,' ','_')
+	SET @Req = ' sp_rename ''TmpObsExport'' ,TProtocol_'  +  replace(@ProtocoleName,' ','_')
 
 	exec ( @req)
 
@@ -170,4 +92,7 @@ END
 
 
 
->>>>>>> 774a4ed94d4fd3ace58237eaed991efecfaac75a
+
+
+
+
