e57af854312df36ae19b83ed2bc4536a2445c23e
romfabbro <romainfabbro@yahoo.fr>
2015-11-19 15:16:12 +0100

    fix View

--- Back/database/View/V_RFIDasFile.sql
+++ Back/database/View/V_RFIDasFile.sql
@@ -7,7 +7,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE view [dbo].[V_dataRFID_as_file]
+ALTER view [dbo].[V_dataRFID_as_file]
 as 
 
 SELECT  e.ID as equipID,S.UnicIdentifier,rfid.FK_Sensor as FK_Sensor,m.Name as site_name,M.Category as site_type,e.StartDate as StartDate,e3.StartDate EndDate,count (distinct chip_code) as nb_chip_code, count (chip_code) as total_scan,Max(rfid.date_) as last_scan
@@ -15,13 +15,13 @@ SELECT  e.ID as equipID,S.UnicIdentifier,rfid.FK_Sensor as FK_Sensor,m.Name as s
 from [ecoReleve_sensor].[dbo].[T_rfid] rfid
 LEFT JOIN Equipment E on e.FK_Sensor = rfid.FK_Sensor and e.Deploy =1 and e.StartDate < rfid.date_
 					and not exists (select * 
-									from  [NewModelERD].[dbo].[Equipment] e2 
+									from  [Equipment] e2 
 									where e2.FK_Sensor = e.FK_Sensor AND e2.StartDate > e.StartDate AND e2.StartDate < rfid.date_)
 LEFT JOIN MonitoredSite M on e.FK_MonitoredSite = M.id
 LEFT JOIN Sensor S on rfid.FK_Sensor = S.ID
 LEFT JOIN Equipment E3 on e3.FK_Sensor = rfid.FK_Sensor and e3.Deploy =0 and e3.StartDate > e.StartDate 
 							and not exists (select * 
-									from  [NewModelERD].[dbo].[Equipment] e4 
+									from  [Equipment] e4 
 									where e4.FK_Sensor = e.FK_Sensor AND e4.StartDate < e3.StartDate and e4.StartDate > e.StartDate)
 where rfid.checked = 0
 GROUP BY S.UnicIdentifier,m.Name,m.Category,e.StartDate,e3.StartDate,e.ID,rfid.FK_Sensor
