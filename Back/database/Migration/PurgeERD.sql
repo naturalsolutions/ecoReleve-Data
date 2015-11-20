3c8578d8c9fee716f50caf76e29f98504d3b782b
Gérald Tibi <gerald_tibi@natural-solutions.eu>
2015-11-20 18:55:12 +0100

    Dernière version script Migration/Export

--- Back/database/Migration/PurgeERD.sql
+++ Back/database/Migration/PurgeERD.sql
@@ -10,18 +10,17 @@ dbcc  CHECKIDENT (StationDynPropValue,reseed,0)
 delete from MonitoredSiteDynPropValue
 dbcc  CHECKIDENT (MonitoredSiteDynPropValue,reseed,0)
 
+delete from Equipment
+dbcc  CHECKIDENT (Equipment,reseed,0)
+
 truncate TABLE ObservationDynPropValue
 dbcc  CHECKIDENT (ObservationDynPropValue,reseed,0)
-
 
 delete from Observation
 dbcc  CHECKIDENT (Observation,reseed,0)
 
 truncate table Individual_Location
 dbcc  CHECKIDENT (Individual_Location,reseed,0)
-
-delete from Equipment
-dbcc  CHECKIDENT (Equipment,reseed,0)
 
 delete from Station_FieldWorker
 dbcc  CHECKIDENT (Station_FieldWorker,reseed,0)
