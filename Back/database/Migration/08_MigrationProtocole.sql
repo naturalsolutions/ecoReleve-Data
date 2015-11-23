580b3b626bb7d40ae884703d17f1386ee6991b87
romfabbro <romainfabbro@yahoo.fr>
2015-11-23 21:03:24 +0100

    up Migration

--- Back/database/Migration/08_MigrationProtocole.sql
+++ Back/database/Migration/08_MigrationProtocole.sql
@@ -18,7 +18,7 @@
 		   ,Original_ID
 		   ,Comments
 		   )
-		   select DISTINCT isnull([DATE],creation_date),S.Name,S.LAT,S.LON,S.ELE,S.precision,FA.ID,1,S.Creation_date,st.id,R.ID,NULL,S.Place,'eReleve_'+CONVERT(VARCHAR,S.TSta_PK_ID),s.Comments
+		   select DISTINCT isnull([DATE],creation_date),S.Name,S.LAT,S.LON,S.ELE,S.precision,FA.ID,1,S.Creation_date,st.id,R.ID,S.TSta_FK_TGeo_ID,S.Place,'eReleve_'+CONVERT(VARCHAR,S.TSta_PK_ID),s.Comments
 		   from [ECWP-eReleveData].dbo.TStations S 
 		   LEFT JOIN fieldActivity FA on FA.Name = S.FieldActivity_Name 
 		   JOIN StationType st ON  st.name = 'standard'
@@ -26,6 +26,9 @@
 		   WHERe NOT EXISTS (select * from Station S2 where S2.LAT = S.LAT AND S2.LON = S.LON AND s2.LAT = S.LAT and S.[DATE] = S2.StationDate)
 		   AND (S.FieldActivity_ID != 27 or S.FieldActivity_ID IS NULL)
 		   AND NOT EXISTS (SELECT * FROM [ECWP-eReleveData].dbo.TStations S2 where S.TSta_PK_ID <> s2.TSta_PK_ID and isnull(S2.LAT,-1) =isnull(S.LAT,-1) and isnull(S2.LON,-1) =isnull(S.LON,-1) and S.[DATE] = S2.[DATE] AND S2.FieldActivity_ID != 27)
+
+
+
 
 INSERT INTO Station_FieldWorker (FK_Station,FK_FieldWorker)
 SELECT s2.ID,FieldWorker1
