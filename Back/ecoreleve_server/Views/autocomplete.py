cfe54ff79f294e4274380062ebc797c5f6415a86
romfabbro <romainfabbro@yahoo.fr>
2015-12-11 13:16:01 +0100

    autocomplete add  distinct

--- Back/ecoreleve_server/Views/autocomplete.py
+++ Back/ecoreleve_server/Views/autocomplete.py
@@ -35,14 +35,15 @@ def autocomplete (request):
     if isinstance(prop,int):
         table = Base.metadata.tables[objName+'DynPropValuesNow']
         query = select([table.c['ValueString'].label('label'),table.c['ValueString'].label('value')]
+            ).distinct(table.c['ValueString']
             ).where(table.c['FK_'+objName+'DynProp']== prop)
         query = query.where(table.c['ValueString'].like('%'+criteria+'%')).order_by(asc(table.c['ValueString']))
     else: 
         if NameValReturn is None:
             NameValReturn = prop
-
+        print(NameValReturn)
         table = Base.metadata.tables[objName]
-        query = select([table.c[NameValReturn].label('value'),table.c[prop].label('label')])
+        query = select([table.c[NameValReturn].label('value'),table.c[prop].label('label')]).distinct(table.c[prop])
         query = query.where(table.c[prop].like('%'+criteria+'%')).order_by(asc(table.c[prop]))
 
     return [dict(row) for row in session.execute(query).fetchall()]
