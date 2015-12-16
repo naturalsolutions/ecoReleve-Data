b8ab66a825232aecdcbf76547b721a2126b646ca
romfabbro <romainfabbro@yahoo.fr>
2015-12-16 12:33:00 +0100

    add index into DB_export

--- Back/database/Export/pr_ExportAfterCreateIndex.sql
+++ Back/database/Export/pr_ExportAfterCreateIndex.sql
@@ -1,8 +1,15 @@
-IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND name = 'pr_ExportAfterCreateIndex')
-	DROP PROCEDURE pr_ExportAfterCreateIndex
+USE [EcoReleve_Export_ECWP]
 GO
 
-CREATE PROCEDURE pr_ExportAfterCreateIndex
+/****** Object:  StoredProcedure [dbo].[pr_ExportAfterCreateIndex]    Script Date: 16/12/2015 08:47:23 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[pr_ExportAfterCreateIndex]
 AS
 BEGIN
 
@@ -19,4 +26,18 @@ BEGIN
 	CREATE INDEX IX_TProtocol_Capture_Individual_fk_individu ON [TProtocol_Capture_Individual](fk_individual)
 
 	CREATE INDEX IX_TProtocol_Capture_Individual_fk_Station ON [TProtocol_Capture_Individual](fk_Station)
+
+	CREATE INDEX IX_TProtocol_Release_Individual_Parent_Observation ON [TProtocol_Release_Individual]([Parent_Observation])
+
+	CREATE INDEX [IX_TIndividual] ON [dbo].[TIndividu] ([ID])
+
+	CREATE INDEX [IX_Sensor_ID_UnicIdentifier] ON [dbo].[TSensor]
+	(
+	[ID] ASC
+	)
+	INCLUDE (UnicIdentifier)
+
 END
+GO
+
+
