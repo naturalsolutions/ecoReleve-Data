a18802d4ca047a001c028d774824e873b6366d26
ktalbi <khaled_talbi@hotmail.com>
2015-12-21 12:10:38 +0100

    merge commit

--- Back/database/Migration/00_InsertTableMigrationConf.sql
+++ Back/database/Migration/00_InsertTableMigrationConf.sql
@@ -174,11 +174,6 @@ select ID,'name_Release_Method',1,'Release_Method' from [MigrationConfigurationP
 select ID,'Name_Record_type',1,'record_type' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Chiroptera detection'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -187,11 +182,6 @@ GO
 select ID,'Name_Exposition',1,'exposition' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -200,11 +190,6 @@ GO
 select ID,'Name_Habitat2',1,'habitat2' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -213,11 +198,6 @@ GO
 select ID,'Name_Hydrography',1,'hydrography' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -226,11 +206,6 @@ GO
 select ID,'Name_micro_habitat',1,'micro_habitat' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -239,11 +214,6 @@ GO
 select ID,'Name_PH_class',1,'ph_class' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -252,11 +222,6 @@ GO
 select ID,'Name_Slope_Class',1,'slope_class' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -265,11 +230,6 @@ GO
 select ID,'Name_soil_texture',1,'soil_texture' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -278,11 +238,6 @@ GO
 select ID,'Name_Substrat',1,'substrat' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -291,11 +246,6 @@ GO
 select ID,'Name_Topography',1,'topography' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -304,11 +254,6 @@ GO
 select ID,'Name_vegetation_series',1,'vegetation_series' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -317,11 +262,6 @@ GO
 select ID,'Name_Global_Abondance_Dom',1,'global_abondance_dom' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology releve'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -330,11 +270,6 @@ GO
 select ID,'Name_Global_Sociability',1,'global_sociability' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology releve'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -343,11 +278,6 @@ GO
 select ID,'Name_Nb_Individuals',1,'nb_individuals' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology releve'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -356,11 +286,6 @@ GO
 select ID,'Name_Phenology_BBCH1',1,'phenology_BBCH1' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology releve'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -369,11 +294,6 @@ GO
 select ID,'Name_Phenology_BBCH2',1,'phenology_BBCH2' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology releve'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -382,11 +302,6 @@ GO
 select ID,'Name_Taxon',1,'taxon' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Phytosociology releve'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -395,11 +310,6 @@ GO
 select ID,'Name_Observation_Tool',1,'observation_tool' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Sighting conditions'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -408,11 +318,6 @@ GO
 select ID,'Name_Weather',1,'weather' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Sighting conditions'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -421,11 +326,6 @@ GO
 select ID,'Name_Wind_Force',1,'wind_force' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Sighting conditions'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -434,11 +334,6 @@ GO
 select ID,'Name_Flora_Main_Species_1',1,'flora_main_species_1' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Simplified habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -447,11 +342,6 @@ GO
 select ID,'Name_Flora_Main_Species_2',1,'flora_main_species_2' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Simplified habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -460,11 +350,6 @@ GO
 select ID,'Name_Flora_Main_Species_3',1,'flora_main_species_3' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Simplified habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -473,11 +358,6 @@ GO
 select ID,'Name_Geomorphology',1,'geomorphology' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Simplified habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -486,11 +366,6 @@ GO
 select ID,'Name_Habitat',1,'habitat' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Simplified habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -499,11 +374,6 @@ GO
 select ID,'Name_Habitat2',1,'habitat2' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Simplified habitat'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -512,11 +382,6 @@ GO
 select ID,'Name_Density_bushes',1,'density_bushes' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -525,11 +390,6 @@ GO
 select ID,'Name_Density_herbs',1,'density_herbs' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -538,11 +398,6 @@ GO
 select ID,'Name_Density_trees',1,'density_trees' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -551,11 +406,6 @@ GO
 select ID,'Name_Exposition',1,'exposition' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -564,11 +414,6 @@ GO
 select ID,'Name_Flora_Main_Species_1',1,'flora_main_species_1' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -577,11 +422,6 @@ GO
 select ID,'Name_Flora_Main_Species_2',1,'flora_main_species_2' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -590,11 +430,6 @@ GO
 select ID,'Name_Flora_Main_Species_3',1,'flora_main_species_3' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -603,11 +438,6 @@ GO
 select ID,'Name_Greeness_bushes',1,'greeness_bushes' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -616,11 +446,6 @@ GO
 select ID,'Name_Greeness_herbs',1,'greeness_herbs' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -629,11 +454,6 @@ GO
 select ID,'Name_Greeness_trees',1,'greeness_trees' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -642,11 +462,6 @@ GO
 select ID,'Name_Moisture',1,'moisture' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -655,11 +470,6 @@ GO
 select ID,'Name_Slope_Class',1,'slope_class' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -668,11 +478,6 @@ GO
 select ID,'Name_Substrat',1,'substrat' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -681,11 +486,6 @@ GO
 select ID,'Name_Topography',1,'topography' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Station description'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -694,11 +494,6 @@ GO
 select ID,'name_taxon',1,'taxon' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Track clue'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -707,11 +502,6 @@ GO
 select ID,'Name_Track_clue',1,'track_clue' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Track clue'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -720,11 +510,6 @@ GO
 select ID,'Name_Behaviour',1,'behaviour' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate group'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -743,11 +528,6 @@ GO
 
 
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -756,11 +536,6 @@ GO
 select ID,'Name_Behaviour',1,'behaviour' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate individual'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -769,11 +544,6 @@ GO
 select ID,'Name_Posture',1,'posture' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate individual'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -782,11 +552,6 @@ GO
 select ID,'name_sex',1,'sex' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate individual'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -795,11 +560,6 @@ GO
 select ID,'Name_Death_Reason',1,'death_reason' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate individual death'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -808,11 +568,6 @@ GO
 select ID,'Name_Death_Time',1,'death_time' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate individual death'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
@@ -821,11 +576,6 @@ GO
 select ID,'Name_Remains',1,'remains' from [MigrationConfigurationProtocoleList] where ProtocoleName = 'Vertebrate individual death'
 GO
 
-<<<<<<< HEAD
-
-=======
-
->>>>>>> c736a1259dfed9e43e5cf39f2f5799e74964caca
  INSERT INTO [dbo].[MigrationConfigurationProtocoleContent] 
 ([fk_ConfigurationProtocole] 
 ,[ColumnName]
