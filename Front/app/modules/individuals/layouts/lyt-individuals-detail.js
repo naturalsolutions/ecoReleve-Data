b934e6b44a53e568bcd47dddcf0e575a74ac4ace
Matheo <matheo_jaouen@natural-solutions.eu>
2016-01-26 12:20:21 +0100

    individual locations

--- Front/app/modules/individuals/layouts/lyt-individuals-detail.js
+++ Front/app/modules/individuals/layouts/lyt-individuals-detail.js
@@ -10,9 +10,10 @@ define([
   'ns_map/ns_map',
   'ns_form/NSFormsModuleGit',
   'ns_navbar/ns_navbar',
+  'ns_modules/ns_com',
 
 ], function($, _, Backbone, Marionette, Swal, Translater, config,
- NsGrid, NsMap, NsForm, Navbar
+ NsGrid, NsMap, NsForm, Navbar, Com
 ) {
 
   'use strict';
@@ -26,6 +27,10 @@ define([
       'click #hideIndivDetails': 'hideDetail',
       'click #showIndivDetails': 'showDetail',
       'click .tab-link': 'displayTab',
+
+      'click table.backgrid th input': 'checkSelectAll',
+
+      'click button#deleteLocations': 'warnDeleteLocations'
     },
     ui: {
       'grid': '#grid',
@@ -37,7 +42,10 @@ define([
       'details': '#indivLeft',
       'mapContainer': '#indivRight',
       'showHideCtr': '#showIndivDetails',
-      'formBtns': '#formBtns'
+      'formBtns': '#formBtns',
+
+      'locationsGrid': '#locationsGrid',
+      'locationsPaginator': '#locationsPaginator',
     },
 
     regions: {
@@ -58,6 +66,7 @@ define([
           model: options.model,
         });
       }
+      this.com = new Com();
     },
 
     reloadFromNavbar: function(model) {
@@ -89,6 +98,7 @@ define([
     },
 
     display: function(model) {
+      this.com = new Com();
       this.model = model;
       this.indivId = parseInt(this.model.get('ID'));
       this.displayForm(this.indivId);
@@ -154,16 +164,101 @@ define([
       this.ui.gridEquipment.html(this.gridEquip.displayGrid());
       this.ui.paginator.html(this.grid.displayPaginator());
       this.ui.paginatorEquipment.html(this.gridEquip.displayPaginator());
+
+      this.displayLocationsGrid();
+    },
+
+    displayLocationsGrid: function() {
+      var _this = this;
+      var locationsCols = [{
+        name: 'ID',
+        label: 'ID',
+        editable: false,
+        renderable: false,
+        cell: 'string'
+      },{
+        name: 'LAT',
+        label: 'latitude',
+        editable: false,
+        cell: 'string'
+      }, {
+        name: 'LON',
+        label: 'longitude',
+        editable: false,
+        cell: 'string'
+      },{
+        name: 'Date',
+        label: 'date',
+        editable: false,
+        cell: 'string'
+      },{
+          editable: true,
+          name: 'import',
+          label: 'Import',
+          cell: 'select-row',
+          headerCell: 'select-all'
+      }];
+
+      this.locationsGrid = new NsGrid({
+        pagingServerSide: false,
+        pageSize: 10,
+        columns: locationsCols,
+        url: config.coreUrl + 'individuals/' + this.indivId  + '/locations',
+        rowClicked: true,
+        com: this.com,
+        idName: 'ID'
+      });
+
+      this.locationsGrid.rowClicked = function(args) {
+        _this.rowClicked(args);
+      };
+
+      this.ui.locationsGrid.html(this.locationsGrid.displayGrid());
+      this.ui.locationsPaginator.html(this.locationsGrid.displayPaginator());
+      this.$el.find('.select-all-header-cell').html('\
+        <button id="deleteLocations" class="btn btn-danger btn-sm">\
+        <span class="reneco reneco-trash"></span>\
+        </button>');
+    },
+
+    //should be in the grid module
+    rowClicked: function(args) {
+      var row = args.row;
+      var evt = args.evt;
+
+      if ($(evt.target).is('input')) {
+        var id = $(evt.target).parent().parent().find('td').html();
+        this.locationsGrid.interaction('selection', id);
+      } else {
+        var id = row.model.get('ID');
+        this.locationsGrid.interaction('focus', id);
+      }
+      //more efficient
+      //row.model.collection.remove(row.model);
     },
 
     displayMap: function() {
       this.map = new NsMap({
-        url: config.coreUrl + 'individuals/' + this.indivId  + '?geo=true',
+        url: config.coreUrl + 'individuals/' + this.indivId  + '/locations?geo=true',
         cluster: true,
         zoom: 3,
         element: 'map',
         popup: true,
+        com: this.com,
+        selection: true
       });
+    },
+
+    focus: function(e) {
+      var tr, id;
+      if ($(e.target).is('td')) {
+        tr = $(e.target).parent();
+      } else if ($(e.target).parent().is('td')) {
+        tr = $(e.target).parent().parent();
+      }
+      id = tr.find('td').first().text();
+      console.log(id);
+      this.locationsGrid.interaction('focus', id);
     },
 
     displayTab: function(e) {
@@ -190,6 +285,7 @@ define([
         parent: this.parent,
         displayDelete: false,
       });
+
       this.nsform.afterDelete = function() {
         /*
         var jqxhr = $.ajax({
@@ -202,6 +298,7 @@ define([
         });*/
       };
     },
+
     hideDetail: function() {
       $(this.ui.details).animate({
         marginLeft: '-60%',
@@ -217,17 +314,87 @@ define([
       });
       this.updateSize('show');
     },
-    updateSize: function(type) {
-      this.map.resize();
-      if (type === 'hide') {
-        $(this.ui.showHideCtr).removeClass('masqued');
-        $(this.ui.mapContainer).removeClass('col-md-7');
-        $(this.ui.mapContainer).addClass('col-md-12');
-      } else {
-        $(this.ui.showHideCtr).addClass('masqued');
-        $(this.ui.mapContainer).removeClass('col-md-12');
-        $(this.ui.mapContainer).addClass('col-md-7');
+
+    warnDeleteLocations: function() {
+
+      var _this = this;
+
+      var mds = this.locationsGrid.grid.getSelectedModels();
+
+      if (!mds.length) {
+        return;
       }
+
+      var callback = function() {
+        _this.deleteLocations(mds);
+      };
+      var opt = {
+        title: 'Are you sure?',
+        text: 'selected locations will be deleted'
+      };
+      this.swal(opt, 'warning', callback);
+    },
+
+    deleteLocations: function(mds) {
+      var _this = this;
+
+      var coll = new Backbone.Collection(mds);
+
+      var params = coll.pluck('ID');
+
+      var url = config.coreUrl + 'individuals/' + this.indivId  + '/locations';
+      $.ajax({
+        url: url,
+        method: 'PUT',
+        data: {'IDs': JSON.stringify(params)},
+        context: this,
+      }).done(function(resp) {
+        _this.map.updateFromServ();
+        for (var i = 0; i < mds.length; i++) {
+          mds[i].collection.remove(mds[i]);
+        }
+      }).fail(function(resp) {
+        this.swal(resp, 'error');
+      });
+    },
+
+    swal: function(opt, type, callback) {
+      var btnColor;
+      switch (type){
+        case 'success':
+          btnColor = 'green';
+          opt.title = 'Success';
+          break;
+        case 'error':
+          btnColor = 'rgb(147, 14, 14)';
+          opt.title = 'Error';
+          break;
+        case 'warning':
+          if (!opt.title) {
+            opt.title = 'warning';
+          }
+          btnColor = 'orange';
+          break;
+        default:
+          return;
+          break;
+      }
+
+      Swal({
+        title: opt.title,
+        text: opt.text || '',
+        type: type,
+        showCancelButton: false,
+        confirmButtonColor: btnColor,
+        confirmButtonText: 'OK',
+        closeOnConfirm: true,
+      },
+      function(isConfirm) {
+        //could be better
+        if (callback) {
+          callback();
+        }
+      });
     },
 
   });
