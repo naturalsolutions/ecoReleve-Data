5d27c6482b3ba97048e1a5745ecbac2c311f0894
romfabbro <romainfabbro@yahoo.fr>
2016-03-10 17:17:16 +0100

    fix auto complete

--- Back/ecoreleve_server/GenericObjets/FrontModules.py
+++ Back/ecoreleve_server/GenericObjets/FrontModules.py
@@ -204,7 +204,7 @@ class ModuleForms(Base):
             , 'lng':threadlocal.get_current_request().authenticated_userid['userlanguage']
             ,'displayValueName': 'valueTranslated'}
             self.dto['options']['startId'] = self.Options
-            self.dto['iconFont'] = 'reneco reneco-thesaurus'
+            self.dto['options']['iconFont'] = 'reneco reneco-thesaurus'
 
     def InputAutocomplete(self):
         if self.Options is not None and self.Options != '':
