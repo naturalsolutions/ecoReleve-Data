15e1bb026867d29f548aef7cd25284f9e7fb1a4a
romfabbro <romainfabbro@yahoo.fr>
2016-06-17 15:41:04 +0200

    BACK | add role in getCurrentUser request

--- Back/ecoreleve_server/Views/user.py
+++ Back/ecoreleve_server/Views/user.py
@@ -1,7 +1,7 @@
 from pyramid.security import NO_PERMISSION_REQUIRED
 from pyramid.view import view_config
 from sqlalchemy import select
-from ..Models import DBSession, User
+from ..Models import DBSession, User, userOAuthDict, USERS
 
 # ------------------------------------------------------------------------------------------------------------------------- #
 @view_config(
@@ -29,12 +29,17 @@ def current_user(request):
     """
     session = request.dbsession
 
-    print(request.authenticated_userid['iss'])
+    currentUserRoleID = userOAuthDict.loc[userOAuthDict['user_id'] == int(request.authenticated_userid['iss']),'role_id'].values[0]
+    currentUserRole = USERS[currentUserRoleID]
+
     query = select([
         User.id.label('PK_id'),
         User.Login.label('fullname')
     ]).where(User.id == request.authenticated_userid['iss'])
-    return dict(session.execute(query).fetchone())
+    response = dict(session.execute(query).fetchone())
+    response['role'] = currentUserRole
+    return response
+
 
 @view_config(
     route_name='users/id',
@@ -43,5 +48,12 @@ def current_user(request):
 def getUser(request) :
     session = request.dbsession
     user_id = request.matchdict['id']
+
+    currentUserRoleID = userOAuthDict.loc[userOAuthDict['user_id'] == int(userid),'role_id'].values[0]
+    currentUserRole = USERS[currentUserRoleID]
+
     query = select([User]).where(User.id == user_id)
-    return dict(session.execute(query).fetchone())
+    response = dict(session.execute(query).fetchone())
+    response['role'] = currentUserRole
+
+    return response
