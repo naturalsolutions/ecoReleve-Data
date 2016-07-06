eb91b789a4437a4daa961db2985f48ba0caa07ba
romfabbro <romainfabbro@yahoo.fr>
2016-07-06 10:57:01 +0200

    quick fix export gpx

--- Back/ecoreleve_server/Views/export.py
+++ Back/ecoreleve_server/Views/export.py
@@ -133,7 +133,7 @@ def views_filter_export(request):
             print('***************')
             splittedColumnLower = {c.name.lower().replace('_',''):c.name for c in table.c}
             print(splittedColumnLower)
-            coll = [table.c[splittedColumnLower['lat']].label('LAT'),table.c[splittedColumnLower['lon']].label('LON'),table.c[splittedColumnLower['creationdate']].label('Date')]
+            coll = [table.c[splittedColumnLower['lat']].label('LAT'),table.c[splittedColumnLower['lon']].label('LON')]
             
             if 'stationname' in splittedColumnLower:
                 coll.append(table.c[splittedColumnLower['stationname']].label('SiteName'))
@@ -141,8 +141,8 @@ def views_filter_export(request):
                 coll.append(table.c[splittedColumnLower['name']].label('SiteName'))
             elif 'sitename' in splittedColumnLower:
                 coll.append(table.c[splittedColumnLower['sitename']].label('SiteName'))
-            #if 'date' in splittedColumnLower:
-                #coll.append(table.c[splittedColumnLower['date']].label('Date'))
+            if 'date' in splittedColumnLower:
+                coll.append(table.c[splittedColumnLower['date']].label('Date'))
 
 
         gene = Generator(viewName,session)
