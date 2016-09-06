9e4734b69649a0cc35daf696d6be21ff2902503c
ktalbi <khaled_talbi@hotmail.com>
2016-09-06 17:21:07 +0200

    fix_user_language

--- Back/ecoreleve_server/Views/user.py
+++ Back/ecoreleve_server/Views/user.py
@@ -16,10 +16,7 @@ def users(request):
     session = request.dbsession
     query = select([
         User.id.label('PK_id'),
-        User.Login.label('fullname'),
-        User.Firstname.label('Firstname'),
-        User.Language.label('Language'),
-        User.Lastname.label('Lastname')
+        User.Login.label('fullname')
     ]).order_by(User.Lastname, User.Firstname)
     return [dict(row) for row in session.execute(query).fetchall()]
 
@@ -48,7 +45,10 @@ def current_user(request):
 
     query = select([
         User.id.label('PK_id'),
-        User.Login.label('fullname')
+        User.Login.label('fullname'),
+        User.Firstname.label('Firstname'),
+        User.Language.label('Language'),
+        User.Lastname.label('Lastname')
     ]).where(User.id == request.authenticated_userid['iss'])
     response = dict(session.execute(query).fetchone())
     response['role'] = currentUserRole
