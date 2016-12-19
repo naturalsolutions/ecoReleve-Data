a574f71672cd82910555ec8ac0f31ecd678ea716
Matheo13 <matheo_jaouen@natural-solutions.eu>
2016-12-19 12:11:55 +0100

    clean

--- Front/app/vendors/jquery.autocompTree.js
+++ Front/app/vendors/jquery.autocompTree.js
@@ -138,6 +138,7 @@
                       }
                     }
                   }
+                  
                 });
                 if (parametres.onInputFocus) {
                   try {
