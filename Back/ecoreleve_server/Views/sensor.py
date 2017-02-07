ca0120b76c56976115fb29a1731d812cc32a519b
romfabbro <romain_fabbro@natural-solutions.eu>
2017-02-07 12:22:31 +0100

    Back: Detail history is traduced

--- Back/ecoreleve_server/Views/sensor.py
+++ Back/ecoreleve_server/Views/sensor.py
@@ -182,6 +182,8 @@ def getSensor(request):
              request_method='GET',
              permission=routes_permission[prefix]['GET'])
 def getSensorHistory(request):
+    from ..Models import thesaurusDictTraduction
+
     session = request.dbsession
     _id = request.matchdict['id']
 
@@ -195,13 +197,17 @@ def getSensorHistory(request):
     result = session.execute(query).fetchall()
     response = []
 
+    userLng = request.authenticated_userid['userlanguage']
     for row in result:
         curRow = OrderedDict(row)
         dictRow = {}
         for key in curRow:
             if curRow[key] is not None:
                 if 'Value' in key:
-                    dictRow['value'] = curRow[key]
+                    if curRow[key] in thesaurusDictTraduction:
+                        dictRow['value'] = thesaurusDictTraduction[curRow[key]][userLng]
+                    else:
+                        dictRow['value'] = curRow[key]
                 elif 'FK' not in key:
                     dictRow[key] = curRow[key]
         dictRow['StartDate'] = curRow[
