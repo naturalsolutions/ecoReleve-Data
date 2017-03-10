d4d6608034cb80eb3a85ed0a02f1bd965584d877
romfabbro <romain_fabbro@natural-solutions.eu>
2017-03-10 11:18:51 +0100

    fix getIdentifer

--- Back/ecoreleve_server/Views/sensor.py
+++ Back/ecoreleve_server/Views/sensor.py
@@ -81,6 +81,7 @@ class SensorsView(DynamicObjectCollectionView):
 
     def __init__(self, ref, parent):
         DynamicObjectCollectionView.__init__(self, ref, parent)
+        self.actions = {'getUnicIdentifier': self.getUnicIdentifier}
         self.__acl__ = context_permissions[ref]
 
     def insert(self):
@@ -92,6 +93,14 @@ class SensorsView(DynamicObjectCollectionView):
             response = self.request.response
             response.text = "This identifier is already used for another sensor"
             pass
+        return response
+
+    def getUnicIdentifier(self):
+        sensorType = self.request.params['sensorType']
+        query = select([Sensor.UnicIdentifier.label('label'), Sensor.ID.label(
+            'val')]).where(Sensor.FK_SensorType == sensorType)
+        response = [OrderedDict(row) for row in self.session.execute(query).fetchall()]
+
         return response
 
 
