2412a335be6d71fd94b71edf796f347bf600269c
Romain Fabbro <romain_fabbro@natural-solutions.eu>
2017-06-06 11:43:46 +0200

    quick fix setProperty (indiv update)

--- Back/ecoreleve_server/GenericObjets/DataBaseObjects.py
+++ Back/ecoreleve_server/GenericObjets/DataBaseObjects.py
@@ -164,8 +164,8 @@ class DbObject(object):
     def setProperty(self, propertyName, value):
         ''' Set object properties (static and dynamic) '''
         if hasattr(self, propertyName):
-                if propertyName in self.__table__.c:
-                    value = parser(value)
+            if propertyName in self.__table__.c:
+                value = parser(value)
                 setattr(self, propertyName, value)
                 self.__properties__[propertyName] = value
 
