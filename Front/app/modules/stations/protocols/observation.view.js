1711deb9655cb0a757e1b17e69dbab5b22067be5
Matheo13 <matheo_jaouen@natural-solutions.eu>
2017-06-06 17:31:48 +0200

    fix

--- Front/app/modules/stations/protocols/observation.view.js
+++ Front/app/modules/stations/protocols/observation.view.js
@@ -40,6 +40,7 @@ define([
       this.model.schema = this.model.get('schema');
       this.model.fieldsets = this.model.get('fieldsets');
       this.model.attributes = this.model.get('data');
+      this.model.set('stationId', this.parentModel.get('stationId'));
 
       this.form = new NsForm({
         modelurl: this.model.urlRoot,
