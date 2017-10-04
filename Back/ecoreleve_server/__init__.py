7cd17931bb61977aa3181bda27b0db569266b2ba
romfabbro <romain_fabbro@natural-solutions.eu>
2017-10-04 11:42:07 +0200

    move init db

--- Back/ecoreleve_server/__init__.py
+++ Back/ecoreleve_server/__init__.py
@@ -63,36 +63,32 @@ def includeme(config):
     config.add_forbidden_view(authn_policy.challenge)
 
 
-def main(global_config, **settings):
-    """ This function initialze DB conection and returns a Pyramid WSGI application. """
+def init_db(config, settings):
+    if 'mssql' in settings['cn.dialect']:
+        settings['sqlalchemy.Export.url'] = settings['cn.dialect'] + \
+            quote_plus(settings['sqlalchemy.Export.url'])
+        engineExport = engine_from_config(
+            settings, 'sqlalchemy.Export.', legacy_schema_aliasing=True)
+    else :
+        engineExport = engine_from_config(
+            settings, 'sqlalchemy.Export.')
 
-    settings['sqlalchemy.Export.url'] = settings['cn.dialect'] + \
-        quote_plus(settings['sqlalchemy.Export.url'])
-    engineExport = engine_from_config(
-        settings, 'sqlalchemy.Export.', legacy_schema_aliasing=True)
-
-    settings['sqlalchemy.default.url'] = settings['cn.dialect'] + \
-        quote_plus(settings['sqlalchemy.default.url'])
-    engine = engine_from_config(
-        settings, 'sqlalchemy.default.', legacy_schema_aliasing=True)
-
-    dbConfig['url'] = settings['sqlalchemy.default.url']
-    dbConfig['wsThesaurus'] = {}
-    dbConfig['wsThesaurus']['wsUrl'] = settings['wsThesaurus.wsUrl']
-    dbConfig['wsThesaurus']['lng'] = settings['wsThesaurus.lng']
-    dbConfig['data_schema'] = settings['data_schema']
+    if 'mssql' in settings['cn.dialect']:
+        settings['sqlalchemy.default.url'] = settings['cn.dialect'] + \
+            quote_plus(settings['sqlalchemy.default.url'])
+        engine = engine_from_config(
+            settings, 'sqlalchemy.default.', legacy_schema_aliasing=True)
+    else :
+        engine = engine_from_config(
+            settings, 'sqlalchemy.default.')
+    
+    config.registry.dbmaker = scoped_session(sessionmaker(bind=engine, autoflush=False))
+    dbConfig['dbSession'] = scoped_session(sessionmaker(bind=engine))
+    config.add_request_method(db, name='dbsession', reify=True)
 
     Base.metadata.bind = engine
     Base.metadata.create_all(engine)
     Base.metadata.reflect(views=True, extend_existing=False)
-
-    config = Configurator(settings=settings)
-    config.include('pyramid_tm')
-    config.include('pyramid_jwtauth')
-
-    config.registry.dbmaker = scoped_session(sessionmaker(bind=engine))
-    dbConfig['dbSession'] = scoped_session(sessionmaker(bind=engine))
-    config.add_request_method(db, name='dbsession', reify=True)
 
     if 'loadExportDB' in settings and settings['loadExportDB'] == 'False':
         print('''
@@ -106,6 +102,24 @@ def main(global_config, **settings):
         BaseExport.metadata.reflect(views=True, extend_existing=False)
         config.registry.dbmakerExport = scoped_session(
             sessionmaker(bind=engineExport))
+
+    return engine
+
+
+def main(global_config, **settings):
+    """ This function initialze DB conection and returns a Pyramid WSGI application. """
+
+    dbConfig['url'] = settings['sqlalchemy.default.url']
+    dbConfig['wsThesaurus'] = {}
+    dbConfig['wsThesaurus']['wsUrl'] = settings['wsThesaurus.wsUrl']
+    dbConfig['wsThesaurus']['lng'] = settings['wsThesaurus.lng']
+    dbConfig['data_schema'] = settings['data_schema']
+
+    config = Configurator(settings=settings)
+    config.include('pyramid_tm')
+    config.include('pyramid_jwtauth')
+
+    init_db(config, settings)
     # Add renderer for JSON objects
     json_renderer = JSON()
     json_renderer.add_adapter(datetime.datetime, datetime_adapter)
@@ -125,8 +139,9 @@ def main(global_config, **settings):
     config.set_root_factory(SecurityRoot)
 
     config.add_subscriber(add_cors_headers_response_callback, NewRequest)
-
     loadThesaurusTrad(config)
+
     add_routes(config)
     config.scan()
+
     return config.make_wsgi_app()
