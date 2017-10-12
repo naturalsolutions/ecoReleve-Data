e79b6f4778dbb00296eba95555cdfda344639446
romfabbro <romain_fabbro@natural-solutions.eu>
2017-10-12 14:51:35 +0200

    up

--- Back/ecoreleve_server/Views/region.py
+++ Back/ecoreleve_server/Views/region.py
@@ -1,10 +1,10 @@
 from ..Models import (
-    Region
+    Region, dbConfig
 )
 import json
 from datetime import datetime
 import pandas as pd
-from sqlalchemy import select, text
+from sqlalchemy import select, text, func
 from traceback import print_exc
 from collections import Counter
 from ..controllers.security import RootCore, context_permissions
@@ -16,6 +16,7 @@ import operator
 from shapely.wkt import loads
 from geojson import Feature, FeatureCollection, dumps
 
+
 class RegionView(CustomView):
     model = Region
 
@@ -26,11 +27,11 @@ class RegionView(CustomView):
             self.objectDB = self.session.query(Region).get(id_)
         except:
             return 'reference not found'
-        
+
         self.__actions__ = {'geoJSON': self.getGeoJson,
-        }
+                            }
         self.__acl__ = context_permissions['release']
-    
+
     def getGeoJson(self):
         return self.objectDB.geom_json
 
@@ -41,6 +42,7 @@ class RegionView(CustomView):
 class RegionTypeView(CustomView):
     model = Region
     item = RegionView
+
     def __init__(self, ref, parent):
         CustomView.__init__(self, ref, parent)
         try:
@@ -54,10 +56,10 @@ class RegionsView(CustomView):
 
     item = RegionView
     colorByTypes = {
-        'administrative': {'fillColor':'#e6e6e6',
-                          'color': '#808080',
+        'administrative': {'fillColor': '#e6e6e6',
+                           'color': '#808080',
                            },
-        'houbara_centered': {'fillColor':'#d279d2',
+        'houbara_centered': {'fillColor': '#d279d2',
                              'color': '#ac39ac',
                              },
     }
@@ -79,37 +81,38 @@ class RegionsView(CustomView):
         results = session.query(Region)
         curStyle = self.colorByTypes['houbara_centered'].copy()
         curStyle.update({
-                'fillOpacity': 0.2,
-                'weight': 3,
-                'opacity': 0.7
-                })
+            'fillOpacity': 0.2,
+            'weight': 3,
+            'opacity': 0.7
+        })
 
         results = results.filter(Region.Region.like('%stan'))
         results = results.all()
-        response ={
+        response = {
             'geojson': [r.geom_json for r in results],
             'style': curStyle
         }
 
         return response
 
-    def getGeomAdministrative(self) :
+    def getGeomAdministrative(self):
         session = self.request.dbsession
         params = self.request.params.mixed()
         results = session.query(Region)
         curStyle = self.colorByTypes['administrative'].copy()
         curStyle.update({
-                'fillOpacity': 0.2,
-                'weight': 3,
-                'opacity': 0.7
-                })
+            'fillOpacity': 0.2,
+            'weight': 3,
+            'opacity': 0.7
+        })
 
         if params and params.get('criteria', None):
             criterias = params.get('criteria')
             for crit in criteria:
-                results = results.filter(getattr(Region,crit['Column']) == crit['Value'])
+                results = results.filter(
+                    getattr(Region, crit['Column']) == crit['Value'])
         results = results.all()
-        response ={
+        response = {
             'geojson': [r.geom_json for r in results],
             'style': curStyle
         }
