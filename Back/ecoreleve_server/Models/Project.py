9cecd95e6561bc99db612765c55fd9392c0ce92c
romfabbro <romain_fabbro@natural-solutions.eu>
2017-10-19 11:17:28 +0200

    agnostic geom start

--- Back/ecoreleve_server/Models/Project.py
+++ Back/ecoreleve_server/Models/Project.py
@@ -1,4 +1,4 @@
-from ..Models import Base
+from ..Models import Base, dbConfig
 from sqlalchemy import (
     Column,
     DateTime,
@@ -20,6 +20,8 @@ from ..utils.parseValue import isEqual, formatValue, parser
 from ..utils.datetime import parse
 from ..GenericObjets.OrmModelsMixin import HasDynamicProperties, GenericType
 from ..utils.geoalchemy import GeometryColumn, Geometry, WKTSpatialElement, Polygon
+from sqlalchemy.ext.declarative import declared_attr
+from geoalchemy2 import Geometry
 
 
 class Project (HasDynamicProperties, Base):
@@ -35,6 +37,13 @@ class Project (HasDynamicProperties, Base):
     FK_Client = Column(Integer, ForeignKey('Client.ID'), nullable=False)
     Project_reference = Column(String(250), nullable=False)
     # area = GeometryColumn(Polygon(2))
+
+    @declared_attr
+    def area(cls):
+        if dbConfig['cn.dialect'] == 'postgres':
+            return Column(Geometry('POLYGON'))
+        if 'mssql' in dbConfig['cn.dialect']:
+            return GeometryColumn(Polygon(2))
 
     Stations = relationship(
         'Station', back_populates='Project', cascade="all, delete-orphan")
