09ed27fbce876547d730b75bfa7b7e42d8ac9c69
romfabbro <romain_fabbro@natural-solutions.eu>
2017-12-12 18:19:13 +0100

    add filter sensor Type on individual

--- Back/ecoreleve_server/Models/List.py
+++ Back/ecoreleve_server/Models/List.py
@@ -191,7 +191,8 @@ class IndividualList(ListObjectWithDynProp):
 
         joinTable = super().GetJoinTable(searchInfo)
 
-        releaseFilter = list(filter(lambda x: x['Column'] == 'LastImported', searchInfo['criteria']))
+        releaseFilter = list(
+            filter(lambda x: x['Column'] == 'LastImported', searchInfo['criteria']))
         if len(releaseFilter) > 0:
             return joinTable
 
@@ -232,6 +233,13 @@ class IndividualList(ListObjectWithDynProp):
             else:
                 query = query.where(eval_.eval_binary_expr(
                     Sensor.UnicIdentifier, criteriaObj['Operator'], criteriaObj['Value']))
+
+        if curProp == 'FK_SensorType':
+            if self.history:
+                query = self.whereInEquipement(query, [criteriaObj])
+            else:
+                query = query.where(eval_.eval_binary_expr(
+                    SensorType.Name, criteriaObj['Operator'], criteriaObj['Value']))
 
         if curProp == 'Status_':
             StatusTable = Base.metadata.tables['IndividualStatus']
@@ -613,15 +621,15 @@ class MonitoredSiteList(ListObjectWithDynProp):
 
 
 class ImportList(Generator):
-    
+
     def __init__(self, SessionMaker):
 
-        joinTable = join(Import, User, Import.FK_User==User.id)
+        joinTable = join(Import, User, Import.FK_User == User.id)
 
         tablecImprt = select([Import,
                               User.fullname.label('Login'),
 
-                            ]).select_from(joinTable
-                                           )
+                              ]).select_from(joinTable
+                                             )
         tablecImprt = tablecImprt.cte()
         super().__init__(tablecImprt, SessionMaker)
