3ad69d5fe7d7ead33036dbab2e8beb527c92bb7c
romfabbro <romain_fabbro@natural-solutions.eu>
2018-01-12 11:21:19 +0100

    fix is null filter on Thesaural input

--- Front/app/ns_modules/ns_filter/filters.js
+++ Front/app/ns_modules/ns_filter/filters.js
@@ -402,14 +402,14 @@
             if (this.firstOperator ) {
                 operatorValue = this.firstOperator;
                 if (this.firstOperator.indexOf('null') != 1){
-                    valeur = 'null';
+                    valeur = null;
                     if (type == 'Number' || type == 'Select' || type == 'LongitudeEditor' || type == 'LatitudeEditor' ){
                         valeur = 1;
                     }
                 }
             }
             var Formdata = {
-                //ColumnType: type,
+                ColumnType: type,
                 Column: fieldName,
                 Operator: operatorValue
             };
@@ -420,7 +420,7 @@
                 schema: schm,
                 defaults: {
                     Column: fieldName,
-                    //ColumnType: type,
+                    ColumnType: type,
                     // For FireFox, select first option
                     Operator: operatorValue,
                     Value: valeur
@@ -473,7 +473,12 @@
                     if (NewOperator.indexOf('null')!=-1) {
                         elVal.addClass('hide');
                         if (this.model.get('ColumnType') != 'Number' || this.model.get('ColumnType') != 'LongitudeEditor' || this.model.get('ColumnType') != 'LatitudeEditor'){
-                            elVal.find('input').val('null').attr('data_value','null').change();
+                            if (this.model.get('ColumnType') == 'AutocompTreeEditor'){
+                                elVal.find('input').val(null).attr('data_value',null).change();
+                            } else {
+                                elVal.find('input').val('null').change();
+                                
+                            }
                         } else {
                             elVal.find('input').val(1).change();
                         }
@@ -701,7 +706,7 @@
 
                     }
                     else {
-                        if (  ( value.Value !== null && value.Value !== '')  || value.Operator == 'is null') {                            
+                        if (  ( value.Value !== null && value.Value !== '')  || value.Operator.indexOf('null') != -1) {                            
                             this.criterias.push(value);
                             currentForm.$el.find('input.filter').addClass('active');
                         }
