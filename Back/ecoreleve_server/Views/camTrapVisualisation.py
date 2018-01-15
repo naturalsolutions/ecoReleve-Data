7cbea371d71b98f90008d2f7b141be536911ade6
jvitus <jeanvitus.albertini@gmail.com>
2018-01-15 02:04:57 +0100

    fix view for monitered site grid camera trap and create view for export camera trap

--- Back/ecoreleve_server/Views/camTrapVisualisation.py
+++ Back/ecoreleve_server/Views/camTrapVisualisation.py
@@ -1,16 +1,17 @@
-from pyramid.response import Response
+from pyramid.response import Response, FileResponse
 from pyramid.view import view_config
-from sqlalchemy import desc, select, func,text, insert, join, Integer, cast, and_, Float, or_,bindparam, update, outerjoin
-from ..Models import Base , dbConfig
+from sqlalchemy import desc, select, func,text, insert, join, Integer, cast, and_, Float, or_,bindparam, update, outerjoin,Table
+from ..Models import Base , dbConfig, CamTrap, Photos
 from ..utils.distance import haversine
 from ..utils.data_toXML import data_to_XML
 from traceback import print_exc
 import pandas as pd
 import datetime, time
-from sqlalchemy.orm import query
+from sqlalchemy.orm import query, joinedload
 from ..controllers.security import routes_permission
-
-
+from tempfile import NamedTemporaryFile
+import zipfile
+import os
 
 @view_config(route_name='cameratrap', renderer='json' ,request_method='GET',permission = routes_permission['rfid']['POST'])
 def allPhotos (request):
@@ -69,8 +70,8 @@ def allPhotos (request):
             print( "fk site "+ str(fkMonitoredSite) )
             print ( "statut change et va devenir 1 " )
             queryStatut = 1
-        if 'equipid' in request.params.keys():
-            fkequipid = request.params['equipid']
+        if 'sessionID' in request.params.keys():
+            fksessionID = request.params['sessionID']
             print ("statut change et va devenir 2 ")
             queryStatut = 2
 
@@ -78,7 +79,7 @@ def allPhotos (request):
         query = text("""
         select
         UnicIdentifier,
-        equipID,
+        sessionID,
         StartDate,
         EndDate,
         count(distinct pk_id) as nbPhotos
@@ -87,7 +88,7 @@ def allPhotos (request):
         join photos as P
         on V.pk_id = P.old_id
         where V.FK_MonitoredSite = :fkmonitoredsite AND V.checked = 1
-        GROUP BY UnicIdentifier, site_name, site_type, StartDate, EndDate, equipID, fk_sensor, V.FK_MonitoredSite""").bindparams(
+        GROUP BY UnicIdentifier, site_name, site_type, StartDate, EndDate, sessionID, fk_sensor, V.FK_MonitoredSite""").bindparams(
         bindparam('fkmonitoredsite',value=fkMonitoredSite))
 
     elif ( queryStatut == 2 ):
@@ -97,11 +98,11 @@ def allPhotos (request):
 		join photos as P
 		on V.pk_id = P.old_id
         where checked = 1
-        AND V.equipID = :equipid
+        AND V.sessionID = :sessionID
         AND V.FK_MonitoredSite = :fkmonitoredsite
 		ORDER BY date_creation ASC""").bindparams(
         bindparam('fkmonitoredsite',value=fkMonitoredSite),
-        bindparam('equipid', value=fkequipid)
+        bindparam('sessionID', value=fksessionID)
         )
     elif ( queryStatut == 3 ):
         query = text(""" select * from Photos,Tags """+ str(queryStrTotalParams))
@@ -122,3 +123,110 @@ def allPhotos (request):
             row['FileName'] = row['FileName'].replace(" ","%20")
 
     return dataResult
+
+@view_config(route_name='getSessionZip', renderer='json' ,request_method='GET',permission = routes_permission['rfid']['POST'])
+def getSessionZip(request):
+
+    fileNameReturned = 'All_Sessions_'
+    sessionid = 0
+    siteid = 0
+
+    viewTable = Table('V_dataCamTrap_With_equipSite',
+                        Base.metadata, autoload=True)
+
+    
+
+    
+    query = select([
+        viewTable.c['pk_id'],
+        viewTable.c['sessionID'],
+        viewTable.c['UnicIdentifier'],
+        viewTable.c['site_name'],
+        viewTable.c['StartDate'],
+        viewTable.c['EndDate'],
+        Photos.Id,
+        Photos.Date,
+        Photos.FileName    
+    ])
+    joinTable = join(Photos , viewTable, Photos.old_id == viewTable.c['pk_id'])
+    query = query.select_from(joinTable)
+    
+    if 'siteid' in request.GET:
+        siteid = request.GET['siteid']
+        query = query.where(
+            viewTable.c['FK_MonitoredSite'] == siteid
+        )
+    if 'sessionid' in request.GET:
+        sessionid = request.GET['sessionid']
+        fileNameReturned = 'Session_'+sessionid+'_'
+        query = query.where(
+            viewTable.c['sessionID'] == sessionid
+        )
+    
+
+
+    session = request.dbsession
+    response = session.execute(query).fetchall()
+
+    fileNameReturned +='for_'+response[0]['site_name']
+    rememberRows = []
+    listFolderName = []
+    respJson = {}
+    for row in response :
+        firstFolder = row['site_name']
+        secondFolder = str(row['StartDate'])[:10]+'_'+str(row['EndDate'])[:10]+'_'+row['UnicIdentifier']
+        thirdFolder = row['FileName']
+        fileName = str(row['Date']).replace(':','-').replace(' ','_')+'_'+str(row['Id'])
+        if firstFolder not in respJson :
+            respJson[firstFolder] = {}
+        
+        if secondFolder not in respJson[firstFolder]:
+            respJson[firstFolder][secondFolder] = {}
+        
+        if thirdFolder not in respJson[firstFolder][secondFolder]:
+            respJson[firstFolder][secondFolder][thirdFolder] = fileName
+        # respJson[firstFolder][secondFolder][thirdFolder].append(fileName)
+
+        elemTmp = []
+        elemTmp.append(row['UnicIdentifier'])
+        startDateParsed = str(row['StartDate'])[:10]
+        elemTmp.append(startDateParsed)
+        endDateParsed = str(row['EndDate'])[:10]
+        elemTmp.append(endDateParsed)
+        elemTmp.append(row['site_name'])
+        folderName = '_'.join(elemTmp)
+        if folderName not in listFolderName :
+            listFolderName.append(folderName)
+
+    fileName = os.path.join(dbConfig['camTrap']['path'],'export',fileNameReturned) 
+    '''
+    TODO :
+        -gen nom aleatoire pour eviter les ecrasements de fichiers si plusieurs personnes GET
+        -penser a mettre en place une purge
+    '''
+    # with NamedTemporaryFile(prefix=fileNameReturned, suffix='.zip' ,dir= os.path.join(dbConfig['camTrap']['path'],'export'),delete=True) as zipfileTmp:
+    zipFile = zipfile.ZipFile(fileName+'.zip', 'w')
+    sizeListFolder = len(listFolderName)
+    for root, dirs, files in os.walk(os.path.join( dbConfig['camTrap']['path'])) :
+        absRoot = root.split('\\')[-1]
+        for i in range(0,sizeListFolder):        
+            if absRoot == listFolderName[i]:
+                folderName = listFolderName[i]
+                (unicIdentifier, startDate, endDate, monitoredSiteName ) = folderName.split('_')            
+                for f in files:
+                    firstFolder = monitoredSiteName
+                    secondFolder = startDate+'_'+endDate+'_'+unicIdentifier
+                    for item in respJson[firstFolder][secondFolder]:
+                        if item.upper() == f.upper():
+                            fileNameForZip = firstFolder+'\\'+secondFolder+'\\'+respJson[firstFolder][secondFolder][item]+f[len(f)-4:]
+                            if os.path.exists(os.path.join(root,f)) :
+                                zipFile.write(os.path.join(root,f),fileNameForZip)
+    zipFile.close()
+    response = FileResponse(
+        os.path.abspath(zipFile.filename),
+        request = request,
+        content_type='application/zip'
+        )
+    response.headers['Content-Disposition'] = ("attachment;filename="+fileNameReturned+'.zip')
+    return response
+
