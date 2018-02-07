7f3f37cd95e6f60237d98e2240a14bd3045860d4
jvitus <jeanvitus.albertini@gmail.com>
2018-02-07 16:47:50 +0100

    add special case when we delete observation that contains mediasFiles

--- Back/ecoreleve_server/Views/protocols.py
+++ Back/ecoreleve_server/Views/protocols.py
@@ -5,8 +5,10 @@ from ..Models import (
     FieldActivity_ProtocoleType,
     fieldActivity,
     ErrorAvailable,
-    sendLog
+    sendLog,
+    MediasFiles
 )
+from pyramid.request import Request
 from sqlalchemy import select, and_, join
 from traceback import print_exc
 from ..controllers.security import RootCore
@@ -56,12 +58,19 @@ class ObservationView(DynamicObjectView):
 
     def delete(self):
         if self.objectDB:
-            # if(self.objectDB.Equipment and self.objectDB.Equipment.checkExistedSensorData()):
-            #     self.request.response.status_code = 409
-            #     return {'protected' : True}
-            # else:
+
             id_ = self.objectDB.ID
             DynamicObjectView.delete(self)
+           
+            # certainly not the better way but don't know where handle it  
+            if 'mediafile' in self.objectDB.ProtocoleType.DynPropNames:
+                mediaItem = None
+                mediaItem = self.session.query(MediasFiles).filter(MediasFiles.FK_Station == self.objectDB.FK_Station).one() 
+                if mediaItem :
+                    delSubReq = Request.blank('/ecoReleve-Core/mediasfiles/'+str(mediaItem.Id) )
+                    delSubReq.method = 'DELETE'
+                    delSubReq.cookies = self.request.cookies
+                    delSubResp = self.request.invoke_subrequest(delSubReq)
         else:
             id_ = None
         response = {'id': id_}
