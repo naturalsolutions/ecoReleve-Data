13740e049aa9afa5bc812268f22159e591fc2066
jvitus <jeanvitus.albertini@gmail.com>
2018-08-27 10:50:08 +0200

    handle pagination on individual locations on backend

--- Back/ecoreleve_server/modules/individuals/individual_locations/individual_locations_resource.py
+++ Back/ecoreleve_server/modules/individuals/individual_locations/individual_locations_resource.py
@@ -7,6 +7,7 @@ from pyramid.traversal import find_root
 from ecoreleve_server.core.base_resource import *
 from ..individual_model import Individual, Individual_Location
 from .inidividual_locations_collection import IndividualLocationsCollection
+from ecoreleve_server.utils.parseValue import integerOrDefault
 
 
 class IndividualLocationsResource(CustomResource):
@@ -52,53 +53,130 @@ class IndividualLocationsResource(CustomResource):
                 'exceed': exceed}
         return data
 
+    def getMinimalParamsForPagination(self,data):
+        '''
+        @request :: A dictionary-like object containing both the parameters from the query string and request body,
+        @returning :: dict
+
+        will find params in request for pagination or set minimal conf
+        params must int
+        ['offset'] :: int >=0,
+        ['per_page'] :: int >= 1
+        '''
+        # data = request.params.mixed()
+        result = {
+            'per_page' : 500,
+            'offset' : 0
+        }
+        
+        if 'per_page' in data:
+            if type(data['per_page']) == list:
+                data['per_page'] = data['per_page'][0]
+
+            result['per_page'] = integerOrDefault(val=data['per_page'],defaultVal=None,positive=True)
+        
+        if 'offset' in data:
+            if type(data['offset']) == list:
+                data['offset'] = data['offset'][0]
+
+            result['offset'] = integerOrDefault(val=data['offset'],defaultVal=0,positive=True)
+
+        elif 'page' in data:
+            if type(data['page']) == list:
+                data['page'] = data['page'][0]
+
+            page = integerOrDefault(val=data['page'],defaultVal=1,positive=True)
+            per_page = result['per_page']
+            result['offset'] = ( page - 1 ) * per_page
+
+        return result
+
+    def getOrderBy(self,data):
+        '''
+        @request :: A dictionary-like object containing both the parameters from the query string and request body,
+        @returning :: list
+        '''
+        result = None
+        if 'order_by' in data :
+            result = json.loads(data['order_by'])
+        else :
+            result = ['Date:desc']
+
+        return result
+    
+    def getFilters(self,data): 
+
+        result = []
+
+        # data = self.request.params.mixed()
+        if 'criteria' in data:
+            result = json.loads(data['criteria'])
+
+        return result
+
     def getLocations(self):
         id_ = self.__parent__.objectDB.ID
         location_collection = IndividualLocationsCollection(session=self.session)
-        data = self.request.params.mixed()
-        if 'criteria' in data:
-            filters = json.loads(data['criteria'])
-        else:
-            filters = []
 
-        filters.append({
+        data = self.request.params.mixed()
+
+
+        defaultFilters = []
+        defaultFilters.append({
             'Column': 'FK_Individual',
             'Operator': '=',
             'Value': id_
         })
 
-        if 'per_page' in data:
-            offset = json.loads(data['offset'])
-            per_page = json.loads(data['per_page'])
-        else:
-            offset = None
-            per_page = None
+        optionsParams = {}
+        optionsParams.update( self.getMinimalParamsForPagination(data) )
+        optionsParams.update({ 'order_by' : self.getOrderBy(data) })
+        optionsParams.update({ 'filters' : self.getFilters(data) })
 
-        if 'geo' in self.request.params:
-            filters.append({
+        optionsParams['filters'].append(defaultFilters[0])
+
+        countAllPosition = 0
+        countAllPosition += location_collection._count(filters=defaultFilters)
+        # defaultFilters.append({
+        #     'Column' : 'type_',
+        #     'Operator' : 'is',
+        #     'Value' : 'station'
+        # })
+        # countAllPosition += location_collection._countStationsPositions(filters=defaultFilters)      
+
+        if 'geo' in data:
+            optionsParams['filters'].append({
                 'Column': 'LAT',
                 'Operator': 'Is not null',
                 'Value': 'null'
             })
-            countResult = location_collection._count(filters=filters)
+            countResult = location_collection._count(filters=optionsParams['filters'])
             dataResult = []
             if countResult < 100000:
-                dataResult = location_collection.search(filters=filters, order_by=['Date:asc'])
+                dataResult = location_collection.search(
+                    filters=optionsParams['filters'],
+                    limit=optionsParams['per_page'],
+                    order_by=optionsParams['order_by'])
             result = self.getGeoJsonResult(dataResult)
 
-            result['total'] = countResult
+            result['total'] = countAllPosition
             # result = gene.get_geoJSON(
             #     criteria, ['ID', 'Date', 'type_', 'precision'], ['Date:asc'])
             # for feature in result['features']:
             #     feature['properties']['Date'] = feature['properties']['Date'].strftime('%Y-%m-%d %H:%M:%S')
             # return result
         else:
-            result = location_collection.search(filters=filters,
-                                 offset=offset,
-                                 limit=per_page,
-                                 order_by=['StationDate:desc'])
+            result = location_collection.search(filters=optionsParams['filters'],
+                                 offset=optionsParams['offset'],
+                                 limit=optionsParams['per_page'],
+                                 order_by=optionsParams['order_by'])
             for row in result:
                 row['Date'] = row['Date'].strftime('%Y-%m-%d %H:%M:%S')
                 row['format'] = 'YYYY-MM-DD HH:mm:ss'
-
-        return result
+        res = {}
+        if 'geo' not in data:
+            res[0] = {'total_entries' : countAllPosition }
+            res[1] = result 
+        else:
+            res = result
+        return res
