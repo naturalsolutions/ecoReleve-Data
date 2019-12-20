c59c9fe297b3fa9fd9bf95840700b3828641efa9
jvitus <jeanvitus.albertini@gmail.com>
2019-12-20 10:38:39 +0100

    update module regions

--- Back/ecoreleve_server/modules/regions/__init__.py
+++ Back/ecoreleve_server/modules/regions/__init__.py
@@ -1,2 +1,15 @@
-from .region_model import *
+from .region_resource import (
+    RegionsResource,
+    RegionResource,
+)
+from ecoreleve_server.core import RootCore
 
+__all__ = [
+    "RegionsResource",
+    "RegionResource"
+]
+
+
+def includeme(config):
+    RootCore.children.append(('regions', RegionsResource))
+    config.scan('.region_view')
