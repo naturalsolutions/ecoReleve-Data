229f00580fe5ec2f44ffe2fa581c5c4e19cc2e82
jvitus <jeanvitus.albertini@gmail.com>
2019-12-20 10:38:25 +0100

    update module observations

--- Back/ecoreleve_server/modules/observations/observation_collection.py
+++ Back/ecoreleve_server/modules/observations/observation_collection.py
@@ -1,11 +1,7 @@
-from sqlalchemy import select, desc, join, func, outerjoin, and_, not_, or_, exists, Table
-
-from ecoreleve_server.core import Base
 from ecoreleve_server.core.base_collection import Query_engine
-from . import Observation
+from ecoreleve_server.database.main_db import Observation
 
 
 @Query_engine(Observation)
 class ObservationCollection:
     pass
-
