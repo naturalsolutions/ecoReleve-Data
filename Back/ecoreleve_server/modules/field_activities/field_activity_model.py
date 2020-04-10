293b2cbfa9efab16548239c3b5aa8125302d4c19
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-04-10 08:30:42 +0200

    update model changes for FieldActivity_ProtocoleType

--- Back/ecoreleve_server/modules/field_activities/field_activity_model.py
+++ Back/ecoreleve_server/modules/field_activities/field_activity_model.py
@@ -1,15 +1,16 @@
-from sqlalchemy import (Boolean,
-                        Column,
-                        ForeignKey,
-                        Integer,
-                        Sequence,
-                        Unicode)
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    Sequence,
+    Unicode
+    )
 
 from ecoreleve_server.core import Base
 from ecoreleve_server.modules.observations import Observation
 
 
-class fieldActivity ( Base ) :
+class fieldActivity(Base):
 
     __tablename__ = 'fieldActivity'
 
@@ -17,13 +18,27 @@ class fieldActivity ( Base ) :
     Name = Column(Unicode(250) , nullable=False)
 
 
-class FieldActivity_ProtocoleType ( Base ) :
+class FieldActivity_ProtocoleType(Base):
 
     __tablename__ = 'FieldActivity_ProtocoleType'
 
-    ID = Column(Integer, Sequence('FieldActivity_ProtocoleType__id_seq'), primary_key=True)
-    FK_fieldActivity = Column(Integer, ForeignKey('fieldActivity.ID'), nullable=False)
-    FK_ProtocoleType = Column(Integer, ForeignKey('ProtocoleType.ID'), nullable=False)
+    FK_fieldActivity = Column(
+        Integer,
+        ForeignKey('fieldActivity.ID'),
+        nullable=False,
+        primary_key=True
+        )
+    FK_ProtocoleType = Column(
+        Integer,
+        ForeignKey('ProtocoleType.ID'),
+        nullable=False,
+        primary_key=True
+        )
+    Order = Column(
+        Integer,
+        nullable=False,
+        server_default='1'
+        )
 
 ProtocoleType = Observation.TypeClass # i puke .... but we don't have choice for now
 
