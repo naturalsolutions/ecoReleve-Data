d08f0458dd770480404b38245c7ae6149a71926c
jvitus <jean-vitus_albertini@natural-solutions.eu>
2020-05-06 16:17:16 +0200

    conflict in import order, need dbconfig for formbuilderview

--- Back/ecoreleve_server/__init__.py
+++ Back/ecoreleve_server/__init__.py
@@ -21,7 +21,7 @@ from .renderers.csvrenderer import CSVRenderer
 from .renderers.pdfrenderer import PDFrenderer
 from .renderers.gpxrenderer import GPXRenderer
 
-from .formbuilder.formbuilder_view import FormBuilderView
+
 # mySubExif = exiftool.ExifTool()
 # mySubExif.start()
 
@@ -98,6 +98,10 @@ def main(global_config, **settings):
     config.add_subscriber(add_cors_headers_response_callback, NewRequest)
     initialize_cameratrap_path(dbConfig, settings)
     loadThesaurusTrad(config)
+
+    # yeah that's dirty but need dbconfig loaded
+    from .formbuilder.formbuilder_view import FormBuilderView
+
     config.add_route('myFormbuilder', dbConfig.get('prefixapi') + '/formbuilder*traverse',
                     factory = 'ecoreleve_server.formbuilder.root_factory_formbuilder' )
     config.add_view( FormBuilderView , route_name='myFormbuilder')
